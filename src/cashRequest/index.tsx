03683fc14aeea012bef7f88534730d330910f57e
wangtao <wangtao@bjblackhole.com>
2023-06-02 17:22:50 +0800

    达成

--- src/cashRequest/index.tsx
+++ src/cashRequest/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import lodash from 'lodash'
 import { useBoolean, useRequest } from 'ahooks';
-import {res} from '../utils'
-
+import {cacheRequest} from '../utils'
 
 async function getArticle(params: any): Promise<{ data: string; time: number }> {
   return new Promise((resolve) => {
@@ -13,19 +12,22 @@ async function getArticle(params: any): Promise<{ data: string; time: number }> 
   });
 }
 
-
+class AA extends React.PureComponent {
+    
+}
 
 
 
 const Article = () => {
-    const callback = (preData, newData) => {
-        console.log('xxx-我不用cash，我偏要用新数据', newData);
-    }
-  const { data, loading } = useRequest( () => res(getArticle, {params: Math.random() * 10}, callback, 'xxx'), {
+    // const callback = (preData, newData) => {
+    //     console.log('xxx-我不用cash，我偏要用新数据', newData);
+    // }
+    const   callback = undefined
+  const { data, loading } = useRequest( () => cacheRequest( getArticle, {params: Math.random() * 10, id: 12}, callback, 'cacheKey' ), {
     // cacheKey: "cacheKey-demo"
   });
 
-  const { data: data2, loading: loading2 } = useRequest(() => res(getArticle, {params2: Math.random() * 3}, callback, 'xxx'), {
+  const { data: data2, loading: loading2 } = useRequest(() => cacheRequest(getArticle, {params2: Math.random() * 3, id: 12}, callback, 'cacheKey2' ), {
     // cacheKey: "cacheKey-demo"
   });
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,110 @@
-const cacheRequest = () => {
-    const cacheMap = new Map()
+import lodash from "lodash";
+// export const createCacheRequest = () => {
+//   const cacheMap = new Map();
 
-    return async  (request, params, callback, cashKey) => {
-        if (cacheMap.has('xx')) {
+//   return async (request, params, callback, cashKey) => {
+//     if (cacheMap.has(cashKey)) {
+//       // request(params).then(res => {
+//       //     cacheMap.set(cashKey, res)
+//       //     callback(cacheMap.get(cashKey), res)
+//       // })
 
-            request(params).then(res => {
-                cacheMap.set('xx', res)
-                callback(cacheMap.get('xx'), res)
-            })
+//       return Promise.resolve(cacheMap.get(cashKey));
+//     }
 
-            return Promise.resolve(cacheMap.get('xx'))
-        }
+//     const resData = await request(params);
+//     cacheMap.set(cashKey, resData);
+//     return resData;
+//   };
+// };
 
-        const resData = await request(params)
-        cacheMap.set('xx', resData)
-        return resData
+// class F {
+//     cacheMap: Map<string, any>;
+
+//     constructor() {
+//         this.cacheMap = new Map();
+//       }
+// }
+const cacheMap = new Map();
+
+// 需要回掉时，必须写在组件中
+// const { data, loading } = useRequest( () => cacheRequest( getArticle, {params: Math.random() * 10, id: 12}, callback, 'cacheKey' ), {
+//     // cacheKey: "cacheKey-demo"
+//   });
+
+// 不用回掉时，可以写在servies中
+// export const getSceneRootNode = async (sceneId: string) => {
+//     return request('blackHole3D/project/scene/' + sceneId, {
+//       method: 'GET',
+//       notWrapperParam: false,
+//       preHandle: false
+//     })
+//   }
+
+class CreateCacheRequest<R, P, K>  {
+    
+  request: (Params: P) => Promise<R>
+  params: P;
+  callback: ((...args: any[]) => void) | undefined;
+  cashKey: K | undefined;
+
+  constructor(request: (Params: P) => Promise<R>, params:P, callback?: (...args: any[]) => void, cashKey?:K ) {
+    this.request = request;
+    this.params = params;
+    this.callback = callback;
+    this.cashKey = String(request) + String(params)
+  }
+
+  // 请求
+  getCacheRequest() {
+
+    const cacheData = cacheMap.has(this.cashKey);
+    // 获取缓存的请求值
+    if (cacheData) {
+        
+      // 如果传了callback，则发起一部分请求，执行回掉
+      if (this.callback && lodash.isFunction(this.callback)) {
+        this.initiateRequest();
+      }
+
+      return Promise.resolve( cacheMap.get(this.cashKey));
     }
+
+    return this.asyncInitiateRequest();
+  }
+
+  // 向后端异步发起请求
+  async asyncInitiateRequest() {
+    const resData = await this.request(this.params);
+    cacheMap.set(this.cashKey, resData);
+    return resData;
+  }
+
+  // 向后端同步发起请求
+  initiateRequest() {
+    this.request(this.params).then((resData: R) => {
+      cacheMap.set(this.cashKey, resData);
+      this.callback?.(cacheMap.get(this.cashKey), resData);
+    });
+  }
 }
-export const res = cacheRequest()
+
+export function cacheRequest <R, P, C, K> (request: (Params: P) => Promise<R>, params: P, callback?: (...args: C[]) => void, cashKey?: K) {
+    const cacheRequestInstance = new CreateCacheRequest(request, params , callback, cashKey);
+    return cacheRequestInstance.getCacheRequest()
+}
+
+async function getArticle(params: any): Promise<{ data: string; time: number }> {
+    return new Promise((resolve) => {
+       setTimeout(() => {
+          console.log('恭喜获得新数据');
+          resolve(params);
+      }, 1000);
+    });
+  }
+
+// const res = await  cacheRequest( getArticle, {params: Math.random() * 10, id: 12} )
+// console.log('res', res)
+
+const res =await  cacheRequest( getArticle, {params: Math.random() * 10, id: 12} )
+console.log('res', res)
